7ce363b9656822b8a783543e20b617e29a77d5c3
Abhijit Roy <abhijit.roy.2404@gmail.com>
2024-05-11 19:35:31 -0400

    Escape API keys in firebase config
    - Escape API keys in firebaseConfig to use template literals
    - Escape auth domain, project ID, and app ID in firebaseConfig
    
    Use template literals to escape API keys and other sensitive credentials in firebaseConfig to avoid exposing them directly in code.

--- yt-web-client/app/firebase/firebase.ts
+++ yt-web-client/app/firebase/firebase.ts
@@ -6,10 +6,10 @@ import { GoogleAuthProvider } from "firebase/auth/cordova";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  appId: process.env.APP_ID,
+  apiKey: `${process.env.API_KEY}`,
+  authDomain: `${process.env.AUTH_DOMAIN}`,
+  projectId: `${process.env.PROJECT_ID}`,
+  appId: `${process.env.APP_ID}`,
 };
 
 // Initialize Firebase
