e95044a44781cab2aa5437b68ce32967c83a47db
Abhijit Roy <abhijit.roy.2404@gmail.com>
2024-05-12 12:20:10 -0400

    Add API function to generate signed URL to upload videos to Google Cloud Storage
    - Add Google Cloud Storage integration
    - Implement function to generate upload URLs
    
    Add ability to upload videos to Google Cloud Storage by integrating the Storage SDK. A new function is implemented that generates a signed upload URL valid for 15 minutes. This will allow the frontend to securely upload videos to the raw video storage bucket.

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -1,14 +1,19 @@
-import * as functions from "firebase-functions";
+import * as v1Functions from "firebase-functions";
+import * as v2Functions from "firebase-functions/v2";
 import {initializeApp} from "firebase-admin/app";
-import * as logger from "firebase-functions/logger";
 import {Firestore} from "firebase-admin/firestore";
+import {Storage} from "@google-cloud/storage";
 
 initializeApp();
 
 const firestore = new Firestore();
+const storage = new Storage();
+const rawVideoBucketName = "aroy-yt-raw-videos";
+const v1Region = "northamerica-northeast1";
+const v2Region = "northamerica-northeast2";
 
-export const createUser = functions
-  .region("northamerica-northeast1").auth.user().onCreate((user) => {
+export const createUser = v1Functions.region(v1Region)
+  .auth.user().onCreate((user) => {
     const userInfo = {
       uid: user.uid,
       email: user.email,
@@ -16,6 +21,33 @@ export const createUser = functions
     };
 
     firestore.collection("users").doc(user.uid).set(userInfo);
-    logger.info(`User created: ${JSON.stringify(userInfo)}`);
+    v1Functions.logger.info(`User created: ${JSON.stringify(userInfo)}`);
     return;
+  });
+
+export const generateUploadUrl = v2Functions.https
+  .onCall({region: v2Region, maxInstances: 1}, async (request) => {
+    // Check if user is authenticated
+    if (!request.auth) {
+      throw new v2Functions.https.HttpsError(
+        "failed-precondition",
+        "The function must be called while authenticated."
+      );
+    }
+
+    const auth = request.auth;
+    const data = request.data;
+    const bucket = storage.bucket(rawVideoBucketName);
+
+    // Generate a unique filename for upload
+    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+    // Get a v4 signed URL for uploading file
+    const [url] = await bucket.file(fileName).getSignedUrl({
+      version: "v4",
+      action: "write",
+      expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+    });
+
+    return [url, fileName];
   });
